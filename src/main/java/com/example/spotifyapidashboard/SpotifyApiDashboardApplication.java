98ed393f5bd67c7ee8386c41a13f7452c7deb90e
Angel <angelatanasov10@gmail.com>
2024-03-28 20:47:21 +0200

    Initial commit

---
+++ src/main/java/com/example/spotifyapidashboard/SpotifyApiDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyapidashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyApiDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotifyapidashboard/SpotifyApiDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyapidashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApiDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
